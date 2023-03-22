c2595fe7eba02418d5751a6da945a9ae6083d3f1
Soumajith <soumajithdas@gmail.com>
2023-03-22 17:58:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const counter = document.querySelector(".counter");
+
+
+const increment = () => {
+    let value = parseInt(counter.innerText);
+    value = value + 1;
+    counter.innerText = value;
+};
+const decrement = () => {
+    let value = parseInt(counter.innerText);
+    value = value - 1;
+    counter.innerText = value;
+};
